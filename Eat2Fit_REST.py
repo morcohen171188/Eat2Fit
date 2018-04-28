fbc18fd0e5437d4bd40ccdc202edeaa0d3aa10e9
mor17_000 <mor171188@gmail.com>
2018-04-28 15:44:57 +0300

    heroku deployment

--- Eat2Fit_REST.py
+++ Eat2Fit_REST.py
@@ -30,7 +30,11 @@ def create_store():
 #get /restaurant/<name> data: {name :}
 @app.route('/restaurant/<string:data>')
 def get_recommended_dishes(data):
-    response = subprocess.call([sys.executable,'rateCalculation.py',data])
+    response = ''
+    try:
+        response = subprocess.check_output([sys.executable,'rateCalculation.py',data])
+    except subprocess.CalledProcessError as e:
+        pass
     response = "".join(map(chr, response))
     response = util.clean_string(response)
     return jsonify(response)
